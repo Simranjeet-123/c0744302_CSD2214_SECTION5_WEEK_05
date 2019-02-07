72a4a72ad6d657270dcd9e361f2a9416cc9ddb56
visualstudio <visualstudio@230-PC16>
2019-02-07 11:56:05 -0500

    final commit

--- c0744302(weekFive)/js/main.js
+++ c0744302(weekFive)/js/main.js
@@ -5,6 +5,14 @@
 }
 function arrays() {
     var pens;
+   
     pens = ['red', 'green', 'pink', 'black', 'orange'];
-    console.log(pens);
+    markers = pens;
+    console.log(markers);
+    var i;
+    for (i = 0; i <markers.length; i++)
+    {
+       alert( i+1+"marker is of color"+markers[i]);
+    }
+    alert("length of array is:" + markers.length);
 }
